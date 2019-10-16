2d4cb4c16c22ab52d2d368f4a6b26270e74aefac
Sergey Leonov <agrael@yandex.ru>
2019-10-16 22:56:57 +0300

    lesson 211

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -59,8 +59,8 @@ export class RecipeEditComponent implements OnInit {
         recipe.ingredients.forEach(ingredient => {
           this.ingredientList.push(
               new FormGroup({
-                'name': new FormControl(ingredient.name),
-                'amount': new FormControl(ingredient.amount)
+                'name': new FormControl(ingredient.name, Validators.required),
+                'amount': new FormControl(ingredient.amount, [Validators.required, Validators.pattern('^[1-9]+[0-9]*$')])
               })
           );
         });
@@ -73,7 +73,6 @@ export class RecipeEditComponent implements OnInit {
       'description': new FormControl(description, Validators.required),
       'ingredients': this.ingredientList
     });
-    console.log(this.ingredientList);
   }
 
   onSubmit(form: FormGroup) {
@@ -88,8 +87,8 @@ export class RecipeEditComponent implements OnInit {
   addIngredient() {
     (<FormArray> this.editRecipeForm.get('ingredients')).push(
         new FormGroup({
-          'name': new FormControl(),
-          'amount': new FormControl()
+          'name': new FormControl(null, Validators.required),
+          'amount': new FormControl(null, [Validators.required, Validators.pattern('^[1-9]+[0-9]*$')])
         })
     );
   }
